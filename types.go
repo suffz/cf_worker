b764aee600883810cf6e3493b08b11b21a36b803
6uf <84757238+suffz@users.noreply.github.com>
2023-12-25 22:32:23 -0400

    Add files via upload

---
+++ types.go
@@ -0,0 +1,94 @@
+package cf_worker
+
+var BasicScript = `export default {
+	/**
+	 * @param {Request} request
+	 */
+	async fetch(request) {
+		let opts = await request.json();
+		if (opts["url"]) {
+			let req_options = {}
+			if (opts["method"] == "GET" || opts["method"] == "HEAD") {
+				req_options = {
+					redirect: 'follow',
+					method: opts["method"],
+					headers: getHeaderObj(opts["headers"]),
+				}
+			} else {
+				req_options = {
+					redirect: 'follow',
+					method: opts["method"],
+					body: 	opts["body"],
+					headers: getHeaderObj(opts["headers"]),
+				}
+			}
+			let r = await fetch(opts["url"], req_options);
+			let options = {true: async function() {
+				let ra = await r.arrayBuffer()
+				return _arrayBufferToBase64(ra)
+			}, false: async function() {
+				let ra = await r.text()
+				return ra
+			}}
+			return Response.json({
+				"url_sent":r.url,
+				"status":r.status,
+				"body":await options[(String(opts["url"]).includes(".png"))](),
+			})
+		}
+	},
+};
+
+function getHeaderObj(o) {
+	var obj = {};
+	for (var value of o) {
+		obj[value["name"]] = value["value"]
+	}
+	return obj;
+}
+
+function _arrayBufferToBase64( buffer ) {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return btoa( binary );
+}`
+
+type Cloud struct {
+	ApiURL string
+	Token  string
+	Body   string
+	Cookie struct {
+		Name  string
+		Value string
+	}
+	Err error
+}
+
+type CloudRequest struct {
+	Script     string // will use detault BasicScript variable
+	JSFileName string
+	WaitTime   int // Defaults To 15
+}
+
+//
+
+type Reply struct {
+	URLSent string `json:"url_sent"`
+	Status  int    `json:"status"`
+	Body    string `json:"body"`
+}
+
+type WorkerRequest struct {
+	URL     string    `json:"url"`
+	Method  string    `json:"method"`
+	Body    string    `json:"body"`
+	Headers []Headers `json:"headers"`
+}
+type Headers struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
---
+++ util.go
@@ -0,0 +1,158 @@
+package cf_worker
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/textproto"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/playwright-community/playwright-go"
+)
+
+func (CloudInfo *CloudRequest) Cloudflare() Cloud {
+	if CloudInfo.WaitTime == 0 {
+		CloudInfo.WaitTime = 15
+	}
+	if CloudInfo.Script == "" {
+		CloudInfo.Script = BasicScript
+	}
+	run, err := playwright.Run()
+	if err != nil {
+		return Cloud{Err: err}
+	}
+	browser, err := run.Chromium.Launch()
+	if err != nil {
+		return Cloud{Err: err}
+	}
+	page, err := browser.NewPage()
+	if err != nil {
+		return Cloud{Err: err}
+	}
+	type Info struct {
+		URL   string
+		TOKEN string
+		ERROR error
+	}
+	var token = make(chan Info, 1)
+	page.Route("**/*", func(r playwright.Route) {
+		ra := r.Request()
+		if ra.URL() == "https://workers.cloudflare.com/playground/api/worker" {
+			buffer, content_type := makeRequestBody(CloudInfo.Script, CloudInfo.JSFileName)
+			ra := ra.Headers()
+			ra["Content-Type"] = content_type
+			r.Continue(playwright.RouteContinueOptions{Headers: ra, PostData: buffer.String()})
+			return
+		} else if strings.Contains(ra.URL(), "cloudflarepreviews.com/") && !strings.Contains(ra.URL(), ".update-preview-token") {
+			for nam, val := range ra.Headers() {
+				if nam == "x-cf-token" {
+					token <- Info{
+						URL:   ra.URL(),
+						TOKEN: val,
+					}
+					break
+				}
+			}
+		}
+		r.Continue()
+	})
+	go func() {
+		time.Sleep(time.Duration(CloudInfo.WaitTime) * time.Second)
+		token <- Info{
+			ERROR: errors.New("Waittime exceeded while fetching api token."),
+		}
+	}()
+	page.Goto("https://workers.cloudflare.com/playground")
+	user := <-token
+
+	C := Cloud{
+		ApiURL: user.URL,
+		Token:  user.TOKEN,
+		Body:   CloudInfo.Script,
+		Cookie: struct {
+			Name  string
+			Value string
+		}{
+			Name:  "token",
+			Value: user.TOKEN,
+		},
+		Err: user.ERROR,
+	}
+
+	if C.Err == nil {
+		os.Mkdir("configs", 0644)
+		path := fmt.Sprintf("configs/config_%v", time.Now().Unix())
+		data, _ := json.MarshalIndent(C, "  ", "    ")
+		os.WriteFile(path, data, 0644)
+	}
+
+	return C
+}
+
+func LoadConfig(path string) (Cloud, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Cloud{}, err
+	}
+
+	var C Cloud
+
+	json.Unmarshal(data, &C)
+
+	return C, nil
+}
+
+func makeRequestBody(body, name string) (*bytes.Buffer, string) {
+	buf := new(bytes.Buffer)
+	writer := multipart.NewWriter(buf)
+	defer writer.Close()
+	h := make(textproto.MIMEHeader)
+
+	h.Set("Content-Disposition", fmt.Sprintf(`form-data; name="%s"; filename="%s"`, name, name))
+	h.Set("Content-Type", "application/javascript+module")
+	body_script, _ := writer.CreatePart(h)
+	body_script.Write([]byte(body))
+
+	h.Set("Content-Disposition", `form-data; name="metadata"; filename="blob"`)
+	h.Set("Content-Type", "application/json")
+	main_module, _ := writer.CreatePart(h)
+	main_module.Write([]byte(fmt.Sprintf(`{"main_module":"%v"}`, name)))
+
+	return buf, writer.FormDataContentType()
+}
+
+func (C *Cloud) BuildRequestBase(METHOD string) (*http.Request, error) {
+	if C.ApiURL == "" {
+		return nil, errors.New("No apiURL found")
+	}
+	req, err := http.NewRequest(METHOD, C.ApiURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.AddCookie(&http.Cookie{Name: C.Cookie.Name, Value: C.Cookie.Value})
+	return req, nil
+}
+
+func (C *Cloud) ApplyDataBody(r *http.Request, body []byte) *http.Request {
+	r.Body = io.NopCloser(bytes.NewBuffer(body))
+	return r
+}
+
+func (C *Cloud) Request(req *http.Request) *http.Response {
+	return send(req, C)
+}
+
+func send(req *http.Request, C *Cloud) *http.Response {
+	resp, _ := http.DefaultClient.Do(req)
+	return resp
+}
+
+func InstallBrowsers() {
+	playwright.Install()
+}
